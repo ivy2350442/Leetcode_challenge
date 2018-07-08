501b13926e0b3dc3e7adeb8ffff49023c9a206b7
ivy2350442 <ivy2350442@gmail.com>
2018-07-09 01:03:28 +0800

    easy

---
+++ 766_Toeplitz_Matrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+
+        for i in range(1, len(matrix), 1):
+            for j in range(1, len(matrix[0]), 1):
+                if matrix[i][j] != matrix[i-1][j-1]:
+                    return False
+
+        return True

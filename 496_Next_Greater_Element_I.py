447cf058207b58ecd9666a9755686c3fdcd9e6d4
ivy2350442 <ivy2350442@gmail.com>
2018-07-15 17:52:03 +0800

    easy_dictionary.get

--- 496_Next_Greater_Element_I.py
+++ 496_Next_Greater_Element_I.py
@@ -6,6 +6,25 @@ class Solution:
         :rtype: List[int]
         """
 
+        #特解
+        #nums2 當作key 存進dictionary ,並將下一個大於自己的值存入
+        dic = {}
+        temp = []
+        for num in nums2:
+            while len(temp) and temp[-1] < num:
+            #while len(temp) != 0 and temp[-1] < num
+                dic[temp.pop()] = num
+            temp.append(num)
+
+        #nums1 進去dictionary 找值
+        ans = []
+        for num in nums1:
+            ans.append(dic.get(num, -1))
+
+        return ans
+
+
+        '''
         ans = []
         for i in range(len(nums1)):
             next_index = nums2.index(nums1[i]) + 1
@@ -22,3 +41,4 @@ class Solution:
                 ans.append(-1)
 
         return ans
+        '''

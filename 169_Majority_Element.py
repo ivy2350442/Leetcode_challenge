5de3ab1250d1da06d72b453ac0690ebf5b002d49
ivy2350442 <ivy2350442@gmail.com>
2018-07-19 21:12:48 +0800

    easy_set

---
+++ 169_Majority_Element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums_set = set(nums)
+
+        for i in nums_set:
+            if nums.count(i) > int(len(nums)/2):
+                return i

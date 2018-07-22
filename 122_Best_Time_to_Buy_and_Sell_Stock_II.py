5f6db9362fe7b8cbdecf039fabb02359200ebf7c
ivy2350442 <ivy2350442@gmail.com>
2018-07-22 23:36:24 +0800

    easy

---
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        ans = 0
+        for i in range(len(prices)-1):
+            if  prices[i] < prices[i+1]:
+                ans += prices[i+1] - prices[i]
+
+        return ans

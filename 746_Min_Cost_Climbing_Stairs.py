26df5632352b11b41dfaf9464d6204c7a823b8d5
ivy2350442 <ivy2350442@gmail.com>
2018-08-01 14:49:00 +0800

    easy_dynamic programming

---
+++ 746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+
+        pre, cur = cost[0], cost[1]
+
+        for i in range(2, len(cost)):
+            pre, cur = cur, min(pre, cur) + cost[i]
+
+        return min(pre, cur)

16a23289c083877424222120f94f87f64cd6aa7e
ivy2350442 <ivy2350442@gmail.com>
2018-08-07 23:45:50 +0800

    easy_and

---
+++ 231_Power_of_Two.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        '''
+        #執行速度過慢
+        if n == 0:
+            return False
+
+        if n == 1:
+            return True
+
+        while n != 2:
+            if (n % 2) != 0:
+                return False
+            n = n / 2
+
+        return Ture
+        '''
+
+        #以二位元做and 的判斷方式較快
+        if n == 0:
+            return False
+
+        if n & (n-1) == 0:
+            return True
+        return False
